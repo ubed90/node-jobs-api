bcec702b7e76d8121cc44e91abf37533c481674d
UbedS <shaikhobaid123@gmail.com>
2023-08-05 12:22:57 +0530

    Default Route Added

--- app.js
+++ app.js
@@ -38,6 +38,11 @@ app.use(cors());
 app.use(xss());
 // extra packages
 
+// Default Pages
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authMiddleware, jobsRouter);
